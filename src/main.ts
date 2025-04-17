5365f6ccb08c8ca2622a3aa3a3f94ef99c1bd9b9
leeminho <twomean99@gmail.com>
2025-04-17 18:20:48 +0900

    feat:회사등록api

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import companiesRouter from "./routes/companiesRouter";
 import { PORT } from "./lib/constance";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/companies", companiesRouter);
 
 app.listen(PORT, () => {
   console.log(`server starting ${PORT}`);
