0bb4f7045ff00ef2149f15e79f4a973df55153f6
leeminho <twomean99@gmail.com>
2025-04-18 16:46:53 +0900

    feat:회사등록, 목록조회 fix: user스키마수정

--- src/controllers/companiesController.ts
+++ src/controllers/companiesController.ts
@@ -1,20 +1,42 @@
 import { Request, Response, NextFunction } from "express";
-import { createCompanyService } from "../services/companiesService";
+import { create } from "superstruct";
+import { SearchByCompany } from "../typings/pagination";
+import { createCompanyStruct } from "../validators/CompanyStructs";
+import * as companyService from "../services/companiesService";
 
-export async function createCompanyController(
+// 회사 등록
+export async function createCompanyHandler(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   try {
-    const { companyName, companyCode } = req.body;
+    const data = create(req.body, createCompanyStruct);
+    const newCompany = await companyService.registerCompany(data);
+    res.status(201).json({ newCompany });
+  } catch (error) {
+    next(error);
+  }
+}
 
-    const newCompany = await createCompanyService({
-      companyCode,
-      companyName,
+// 회사 목록조회
+export async function getCompanyListHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { page = 1, pageSize = 3, orderBy, keyword, searchBy } = req.query;
+
+    const result = await companyService.getCompanyList({
+      page: Number(page),
+      pageSize: Number(pageSize),
+      orderBy: orderBy as "recent" | "oldest",
+      keyword: keyword as string,
+      searchBy: searchBy as SearchByCompany,
     });
 
-    return res.status(201).json({ newCompany });
+    res.status(200).json(result);
   } catch (error) {
     next(error);
   }
--- src/validators/CompanyStructs.ts
+++ src/validators/CompanyStructs.ts
@@ -1,11 +1,6 @@
-import { object, string, refine } from "superstruct";
+import { object, string } from "superstruct";
 
-export const CompanyStruct = object({
-  name: string(),
-  businessNumber: refine(string(), "businessNumber", (value) =>
-    /^\d{3}-\d{2}-\d{5}$/.test(value)
-  ),
-  ceoName: string(),
-  address: string(),
-  phone: string(),
+export const createCompanyStruct = object({
+  companyName: string(),
+  companyCode: string(),
 });
