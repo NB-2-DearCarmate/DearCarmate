925b4ad7b7ec6326e63a63a0e6485609a6057633
leeminho <twomean99@gmail.com>
2025-04-21 17:44:13 +0900

    feat:관리자인증

--- src/routes/companiesRouter.ts
+++ src/routes/companiesRouter.ts
@@ -6,13 +6,20 @@ import {
   getUserByCompaniesHandler,
   updateCompanyHandler,
 } from "../controllers/companiesController";
+import { adminMiddleware } from "../middlewares/adminAuthMiddleware";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.post("/", createCompanyHandler);
-router.get("/", getCompanyListHandler);
-router.get("/users", getUserByCompaniesHandler);
-router.patch("/:id", updateCompanyHandler);
-router.delete("/:id", deleteCompanyHandler);
+router.post("/", authMiddleware, adminMiddleware, createCompanyHandler);
+router.get("/", authMiddleware, adminMiddleware, getCompanyListHandler);
+router.get(
+  "/users",
+  authMiddleware,
+  adminMiddleware,
+  getUserByCompaniesHandler
+);
+router.patch("/:id", authMiddleware, adminMiddleware, updateCompanyHandler);
+router.delete("/:id", authMiddleware, adminMiddleware, deleteCompanyHandler);
 
 export default router;
