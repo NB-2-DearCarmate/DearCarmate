2043dbe49835e837e216edd58cfd48d1d3f0382f
leeminho <twomean99@gmail.com>
2025-04-21 17:08:18 +0900

    feat:companies router

--- src/routes/companiesRouter.ts
+++ src/routes/companiesRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
   createCompanyHandler,
+  deleteCompanyHandler,
   getCompanyListHandler,
   getUserByCompaniesHandler,
+  updateCompanyHandler,
 } from "../controllers/companiesController";
 
 const router = Router();
@@ -10,5 +12,7 @@ const router = Router();
 router.post("/", createCompanyHandler);
 router.get("/", getCompanyListHandler);
 router.get("/users", getUserByCompaniesHandler);
+router.patch("/:id", updateCompanyHandler);
+router.delete("/:id", deleteCompanyHandler);
 
 export default router;
