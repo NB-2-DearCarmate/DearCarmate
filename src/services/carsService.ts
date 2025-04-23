b9074e55fe6dc1441340f8efe090a02f3230e325
Seung <ksh30916@naver.com>
2025-04-23 11:05:42 +0900

    feat: 코드 컨벤션 및 조회 API 생성

--- src/services/carsService.ts
+++ src/services/carsService.ts
@@ -1,17 +1,37 @@
 import NotFoundError from "../errors/NotFoundError";
 import BadRequestError from "../errors/BadRequestError";
-import ForbiddenError from "../errors/ForbiddenError";
-import { carRepository } from "../Repositories/carsRepository";
-import { CreateCarDTO } from "../typings/car";
+import { CarPaginationParams } from "../typings/pagination";
+import carRepository from "../repositories/carsRepository";
+import { CreateCarDTO, CarList } from "../typings/car";
 
-export const carService = {
-  async createCar(carData: CreateCarDTO) {
-    // 차량 번호 중복 체크
-    const existingCar = await carRepository.findByCarNumber(carData.carNumber);
-    if (existingCar) {
-      throw new BadRequestError("이미 등록된 차량 번호입니다.");
-    }
+async function createCar(carData: CreateCarDTO) {
+  const existingCar = await carRepository.findByCarNumber(carData.carNumber);
+  if (existingCar) {
+    throw new BadRequestError("이미 등록된 차량 번호입니다.");
+  }
 
-    return carRepository.createCar(carData);
-  },
+  return carRepository.createCar(carData);
+}
+
+async function getCarList(params: CarPaginationParams): Promise<CarList> {
+  return carRepository.getCarList(params);
+}
+
+async function getCarById(id: number) {
+  const car = await carRepository.getCarById(id);
+  if (!car) {
+    throw new NotFoundError(id);
+  }
+  return car;
+}
+
+async function getAllCarModels() {
+  return getAllCarModels();
+}
+
+export default {
+  createCar,
+  getCarList,
+  getCarById,
+  getAllCarModels,
 };
