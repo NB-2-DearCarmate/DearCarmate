e258f53287137846493cda881f6db1a47b4141fc
hapyvirus <serenity2011@naver.com>
2025-04-29 11:45:15 +0900

    feat: router asyncHandler 추가

--- src/routes/contractRouter.ts
+++ src/routes/contractRouter.ts
@@ -9,15 +9,16 @@ import {
   updateContract,
 } from "../controllers/contractController";
 import authMiddleware from "../middlewares/authMiddleware";
+import { asyncHandler } from "../lib/asyncHandler";
 
 const contractRouter = Router();
 
-contractRouter.get("/", authMiddleware, getContractList);
-contractRouter.get("/customers", authMiddleware, getCustomerList);
-contractRouter.get("/cars", authMiddleware, getCarList);
-contractRouter.get("/users", authMiddleware, getUserList);
-contractRouter.post("/", authMiddleware, createContract);
-contractRouter.patch("/:id", authMiddleware, updateContract);
-contractRouter.delete("/:id", authMiddleware, deleteContract);
+contractRouter.get("/", authMiddleware, asyncHandler(getContractList));
+contractRouter.get("/customers", authMiddleware, asyncHandler(getCustomerList));
+contractRouter.get("/cars", authMiddleware, asyncHandler(getCarList));
+contractRouter.get("/users", authMiddleware, asyncHandler(getUserList));
+contractRouter.post("/", authMiddleware, asyncHandler(createContract));
+contractRouter.patch("/:id", authMiddleware, asyncHandler(updateContract));
+contractRouter.delete("/:id", authMiddleware, asyncHandler(deleteContract));
 
 export default contractRouter;
