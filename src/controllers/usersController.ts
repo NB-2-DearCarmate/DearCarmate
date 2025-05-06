22a1f046f586f161a5a4681d0679e574ac99a9f7
Zen-nojen <jiyoyo1234@naver.com>
2025-05-07 08:37:08 +0900

    한글 반환 Controller 코드 수정

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,30 +1,35 @@
 import { Request, Response } from "express";
 import UserService from "../services/usersService";
 import { AuthenticatedUserRequest } from "../typings/express";
-import {
-  CreateUserRequest,
-  UpdateMyInfoRequest,
-  UserResponse,
-} from "../dto/usersDTO";
+import { CreateUserRequest, UpdateMyInfoRequest } from "../dto/usersDTO";
 import { UpdateMyInfoStruct } from "../validators/UsersStructs";
 import { IdParamsStruct } from "../validators/CommonStruct";
 import { create } from "superstruct";
+import { toUserKoResponse } from "../dto/toUserKoResponse";
+import { UserKoResponse } from "../dto/UserKoResponse";
+import { Prisma } from "@prisma/client";
 
 const userService = new UserService();
 
 // 회원가입
 export const createUserHandler = async (
-  req: Request<{}, UserResponse, CreateUserRequest>,
-  res: Response<UserResponse | { message: string }>
+  req: Request<{}, UserKoResponse, CreateUserRequest>,
+  res: Response<UserKoResponse>
 ): Promise<void> => {
   const user = await userService.createUser(req.body);
-  res.status(201).json(user);
+  res
+    .status(201)
+    .json(
+      toUserKoResponse(
+        user as Prisma.UserGetPayload<{ include: { company: true } }>
+      )
+    );
 };
 
 // 정보 조회
 export const getMyInfoHandler = async (
   req: AuthenticatedUserRequest,
-  res: Response<UserResponse | { message: string }>
+  res: Response<UserKoResponse | { message: string }>
 ): Promise<void> => {
   if (!req.user) {
     res.status(401).json({ message: "로그인이 필요합니다" });
@@ -32,13 +37,19 @@ export const getMyInfoHandler = async (
   }
 
   const user = await userService.getMyInfo(Number(req.user.id));
-  res.status(200).json(user);
+  res
+    .status(200)
+    .json(
+      toUserKoResponse(
+        user as Prisma.UserGetPayload<{ include: { company: true } }>
+      )
+    );
 };
 
 // 정보 수정
 export const updateMyInfoHandler = async (
   req: AuthenticatedUserRequest,
-  res: Response<UserResponse | { message: string }>
+  res: Response<UserKoResponse | { message: string }>
 ): Promise<void> => {
   if (!req.user) {
     res.status(401).json({ message: "로그인이 필요합니다" });
@@ -48,7 +59,13 @@ export const updateMyInfoHandler = async (
   const data = create(req.body, UpdateMyInfoStruct) as UpdateMyInfoRequest;
   const user = await userService.updateMyInfo(Number(req.user.id), data);
 
-  res.status(200).json(user);
+  res
+    .status(200)
+    .json(
+      toUserKoResponse(
+        user as Prisma.UserGetPayload<{ include: { company: true } }>
+      )
+    );
 };
 
 // 회원탈퇴
